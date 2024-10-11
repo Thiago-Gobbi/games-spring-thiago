971fd14082754f5b402b53ea74f7dcccf648f8c5
Thiago Gobbi <thiagogobbi@hotmail.com>
2024-10-11 14:37:00 +0000

    versão

--- app/src/main/java/application/model/Categoria.java
+++ app/src/main/java/application/model/Categoria.java
@@ -1,6 +1,5 @@
 package application.model;
 
-import java.lang.annotation.Inherited;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,6 +17,33 @@ public class Categoria {
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
+    private String nome;
 
+    @OneToMany(mappedBy = "categoria")
+    private Set<Jogo> jogos = new HashSet<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Jogo> getJogos() {
+        return jogos;
+    }
+
+    public void setJogos(Set<Jogo> jogos) {
+        this.jogos = jogos;
+    }
 }
---
+++ app/src/main/java/application/model/Jogo.java
@@ -0,0 +1,68 @@
+package application.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "jogos")
+public class Jogo {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String titulo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_genero")
+    private Categoria categoria;
+
+    @ManyToMany
+    @JoinTable(
+        name = "jogos_possuem_plataformas",
+        joinColumn = @JoinColumn(name = "id_plataformas"),
+        iverseJoinColumns = @JoinColumn(name = "id_plataformas"))
+    private Set<Plataforma> platafomas = new HashSet<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Set<Plataforma> getPaltaformas() {
+        return plataforms;
+    }
+
+    public void setPlataforms(Set<Plataforma> plataformas) {
+        this.plataformas = plataformas;
+    }
+}
